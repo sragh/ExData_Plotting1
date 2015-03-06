e56ab673e47fc9e4be3c76dbadfdc6b36600b5f8
SR <=sragh@users.noreply.github.com>
2015-03-06 09:24:55 -0500

    minor edit

--- plot4.R
+++ plot4.R
@@ -24,7 +24,7 @@ xlab = "", ylab = "Energy sub metering")
 lines(DateTime, Sub_metering_2, col = "red")
 lines(DateTime, Sub_metering_3, col = "blue")
 # Remove the border of legend here.
-legend("toprigaht",
+legend("topright",
 bty = "n",
 col = c("black", "red", "blue"),
 c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
